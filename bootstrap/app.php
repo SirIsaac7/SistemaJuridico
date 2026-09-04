e63e8b3b98a9b4983c6d0a3c80305686dc02055b
SirIsaac7 <isaacat7@gmail.com>
2026-09-03 21:19:48 -0400

    Se quito la saturacion de app.php/boostrap para produccion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+//use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -18,11 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*'); // esto es de ngrok
         $middleware->web(append: [
             EnsureDeviceTokenCookie::class,
             HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
+            //AddLinkHeadersForPreloadedAssets::class,
             EnsureUserIsActive::class,
             EnsureAuthorizedDeviceSession::class,
         ]);
